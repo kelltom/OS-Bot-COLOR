329f40c8480a5ba6e63b9e6359ae6564658a8606
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-06-24 14:23:20 -0400

    Update main_controller.py
    Linting

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -2,5 +2,5 @@
 The controller for the main GUI.
 
 This controller should control button clicks for things like
-selecting a script, and managing the main view. 
+selecting a script, and managing the main view.
 '''
