5f0c4cc927bf59b2181606168b765830989b598b
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-06-28 13:25:06 -0400

    Modified control buttons

--- src/view/script_views/custom_views/info_frame.py
+++ src/view/script_views/custom_views/info_frame.py
@@ -47,10 +47,10 @@ class InfoFrame(customtkinter.CTkFrame):
 
         # -- right-side control buttons
         # -- images
-        img_size = 20
+        img_size = 18
         img_play = ImageTk.PhotoImage(Image.open(f"{PATH}/images/play.png").resize((img_size, img_size)), Image.ANTIALIAS)
-        img_pause = ImageTk.PhotoImage(Image.open(f"{PATH}/images/pause.png").resize((img_size, img_size)), Image.ANTIALIAS)
-        img_stop = ImageTk.PhotoImage(Image.open(f"{PATH}/images/stop.png").resize((img_size, img_size)), Image.ANTIALIAS)
+        img_stop = ImageTk.PhotoImage(Image.open(f"{PATH}/images/stop2.png").resize((img_size, img_size)), Image.ANTIALIAS)
+        img_restart = ImageTk.PhotoImage(Image.open(f"{PATH}/images/restart.png").resize((img_size, img_size)), Image.ANTIALIAS)
 
         self.lbl_controls_title = customtkinter.CTkLabel(master=self,
                                                          text="Controls",
@@ -60,15 +60,22 @@ class InfoFrame(customtkinter.CTkFrame):
 
         self.btn_play = customtkinter.CTkButton(master=self,
                                                 text="Play [F1]",
+                                                text_color="white",
                                                 image=img_play)
-        self.btn_play.grid(row=1, column=1, pady=10, padx=20, sticky="n")
-
-        self.btn_pause = customtkinter.CTkButton(master=self,
-                                                 text="Pause [F2]",
-                                                 image=img_pause)
-        self.btn_pause.grid(row=2, column=1, pady=10, padx=20, sticky="n")
+        self.btn_play.grid(row=1, column=1, pady=10, padx=20, sticky="nsew")
 
         self.btn_abort = customtkinter.CTkButton(master=self,
                                                  text="Stop [ESC]",
+                                                 text_color="white",
+                                                 fg_color="#910101",
+                                                 hover_color="#690101",
                                                  image=img_stop)
-        self.btn_abort.grid(row=3, column=1, pady=10, padx=20, sticky="n")
+        self.btn_abort.grid(row=2, column=1, pady=10, padx=20, sticky="nsew")
+
+        self.btn_restart = customtkinter.CTkButton(master=self,
+                                                   text="Restart [F4]",
+                                                   text_color="white",
+                                                   fg_color="#d97b00",
+                                                   hover_color="#b36602",
+                                                   image=img_restart)
+        self.btn_restart.grid(row=3, column=1, pady=10, padx=20, sticky="nsew")
