3d362fd9b521ac1c1ac6bb59caebf736dda52b31
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-06-29 15:03:55 -0400

    Wrote polling function

--- src/controller/cerberus_controller.py
+++ src/controller/cerberus_controller.py
@@ -1,3 +1,4 @@
+from threading import Thread
 
 
 class CerberusController(object):
@@ -7,29 +8,57 @@ class CerberusController(object):
         '''
         self.model = model
         self.view = view
+        self.should_poll = False
+        self.thread = None
+
+    def poll(self):
+        '''
+        Poll the model for data.
+        '''
+        while self.should_poll:
+            # Check if there is data in the queue.
+            if self.model.queue.qsize() > 0:
+                # Get the data from the queue.
+                data = self.model.queue.get()
+                # Determine if data is a status update or a script output.
+                if data[0] == 'status':
+                    # Update the status on the view.
+                    self.view.update_status(data[1])
+                    if data[1] in [3, 4]:  # Stopped or Idle
+                        self.should_poll = False
+                elif data[0] == 'output':
+                    # Update the log on the view.
+                    self.view.update_log(data[1])
 
     def play_pause(self):
         '''
         Play/pause btn clicked on view.
         '''
+        prev_status = self.model.status
         self.model.play_pause()
-        self.view.update_status(self.model.status)
+        self.view.update_status(self.model.status)  # TODO: Remove
+        self.should_poll = True
+        # If the bot was stopped or idle, start a new thread,
+        # as the old thread will be dead.
+        if prev_status in [3, 4]:
+            self.thread = Thread(target=self.poll)
+            self.thread.start()
 
     def stop(self):
         '''
         Stop btn clicked on view.
         '''
         self.model.stop()
+        # TODO: Remove
         self.view.update_status(self.model.status)
+        self.should_poll = False
 
     def restart(self):
         '''
         Restart btn clicked on view.
         '''
         self.model.restart()
-        self.view.update_status(self.model.status)
-
-    # TODO:
-    # I need a function that, when the bot is running, constantly checks a queue within the model for
-    # messages to be sent to the view. This queue will be populated via the model's main loop.
-    # The view will then update the progress bar and log accordingly.
+        self.view.update_status(self.model.status)  # TODO: Remove
+        # TODO: Verify this code. Should I be restarting the thread? Will the old thread die?
+        self.thread = Thread(target=self.poll)
+        self.thread.start()
