181e322322d8224f3c231337f31393f44b237cab
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-07-04 13:43:18 -0400

    Simplified script btn handlers
    Made it so that one function handles all script selections.

--- src/main.py
+++ src/main.py
@@ -55,21 +55,21 @@ class App(customtkinter.CTk):
         self.btn_firemaking = customtkinter.CTkButton(master=self.frame_left,
                                                       text="Firemaking",
                                                       fg_color=("gray75", "gray30"),
-                                                      command=self.show_firemaking_view)
+                                                      command=lambda: self.show_frame("Firemaking", self.btn_firemaking))
         self.btn_firemaking.grid(row=2, column=0, pady=10, padx=20)
         self.button_list.append(self.btn_firemaking)
 
         self.btn_woodcutting = customtkinter.CTkButton(master=self.frame_left,
                                                        text="Woodcutting",
                                                        fg_color=("gray75", "gray30"),
-                                                       command=self.show_woodcutting_view)
+                                                       command=None)
         self.btn_woodcutting.grid(row=3, column=0, pady=10, padx=20)
         self.button_list.append(self.btn_woodcutting)
 
         self.btn_zulrah = customtkinter.CTkButton(master=self.frame_left,
                                                   text="Zulrah",
                                                   fg_color=("gray75", "gray30"),
-                                                  command=self.show_zulrah_view)
+                                                  command=lambda: self.show_frame("Zulrah", self.btn_zulrah))
         self.btn_zulrah.grid(row=4, column=0, pady=10, padx=20)
         self.button_list.append(self.btn_zulrah)
 
@@ -80,16 +80,16 @@ class App(customtkinter.CTk):
         self.switch.select()
 
         # ============ frame_right ============
-        self.view_list = []
+        self.view_list = {}
         # Firemaking
         self.firemaking_frame = FiremakingView(parent=self.frame_right)
         self.firemaking_model = Firemaking()
         self.firemaking_controller = BotController(model=self.firemaking_model, view=self.firemaking_frame)
         self.firemaking_frame.set_controller(self.firemaking_controller)
-        self.view_list.append(self.firemaking_frame)
+        self.view_list["Firemaking"] = self.firemaking_frame
         # Zulrah
         self.zulrah_frame = ZulrahView(parent=self.frame_right)
-        self.view_list.append(self.zulrah_frame)
+        self.view_list["Zulrah"] = self.zulrah_frame
 
     # ============ Script button handlers ============
     def hide_all_frames(self):
@@ -97,22 +97,13 @@ class App(customtkinter.CTk):
         # set all buttons to default color
         for button in self.button_list:
             button.config(fg_color=("gray75", "gray30"))
-        for view in self.view_list:
+        for view in self.view_list.values():
             view.pack_forget()
 
-    def show_firemaking_view(self):
+    def show_frame(self, name, btn):
         self.hide_all_frames()
-        self.btn_firemaking.config(fg_color=self.btn_firemaking.hover_color)
-        self.firemaking_frame.pack(fill="both", expand=1)
-
-    def show_woodcutting_view(self):
-        self.hide_all_frames()
-        self.btn_woodcutting.config(fg_color=self.btn_woodcutting.hover_color)
-
-    def show_zulrah_view(self):
-        self.hide_all_frames()
-        self.btn_zulrah.config(fg_color=self.btn_zulrah.hover_color)
-        self.zulrah_frame.pack(fill="both", expand=1)
+        btn.config(fg_color=btn.hover_color)
+        self.view_list[name].pack(fill="both", expand=1)
 
     # ============ Misc handler ============
     def button_event(self):
