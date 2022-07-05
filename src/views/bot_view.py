f1c1bf419501ccc797841604587f2289ebe74da1
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-07-05 13:22:45 -0400

    Removed references to options in BotView

--- src/views/bot_view.py
+++ src/views/bot_view.py
@@ -21,7 +21,6 @@ class BotView(customtkinter.CTkFrame):
 
         # configure grid layout (3x1)
         self.rowconfigure(0, weight=0)  # info row will not resize
-        self.rowconfigure(1, weight=0)  # options row will not resize
         self.rowconfigure(2, weight=1)  # log row will resize
         self.columnconfigure(0, weight=1)
 
@@ -38,9 +37,9 @@ class BotView(customtkinter.CTkFrame):
 
         self.controller = None
 
-    def setup(self, controller, options_view: AbstractOptions, title, description):
+    def setup(self, controller, title, description):
         '''
-        Sets up the view with the given controller and options view. Also sets the title and
+        Sets up the view (and child views) with the given controller. Also sets the title and
         description for the info frame.
         '''
         self.controller = controller
@@ -49,17 +48,6 @@ class BotView(customtkinter.CTkFrame):
         self.frame_info.setup(title=title, description=description)
 
         self.frame_output_log.set_controller(controller=controller)
-
-        self.frame_options = options_view
-        self.frame_options.grid(row=1, column=0, padx=15, sticky="nsew")
-        self.frame_options.set_controller(controller=controller)
-
-    def get_settings(self) -> dict:
-        '''
-        Extracts all settings from view to a dictionary.
-        '''
-        # TODO: make a call to frame_options.get_settings()
-        return {}
 
     def update_status(self, status):
         '''
