67e02851766c1472e5c5189826c7ed22e7ce5f3e
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-07-11 14:13:26 -0400

    Improved button appearance
    Made some adjustments to improve the balance of widgets.

--- src/view/default_view.py
+++ src/view/default_view.py
@@ -16,13 +16,19 @@ class DefaultView(customtkinter.CTkFrame):
         self.grid_rowconfigure(4, weight=0)  # - Copy text
         self.grid_rowconfigure(5, weight=1)  # Spacing
 
-        # title label
+        # Images
+        PATH = pathlib.Path(__file__).parent.parent.resolve()
+        img_size = 18
+        full_img_width = img_size + 5
+        self.img_copy = ImageTk.PhotoImage(Image.open(f"{PATH}/images/copy.png").resize((img_size, img_size)), Image.ANTIALIAS)
+
+        # Title label
         self.label_title = customtkinter.CTkLabel(master=self,
                                                   text="Welcome to OSNR Bot!",
                                                   text_font=("Roboto Medium", 18))
-        self.label_title.grid(row=1, column=0, sticky="nwes", padx=15, pady=15)
+        self.label_title.grid(row=1, column=0, sticky="nwes", padx=15, pady=(0, 15))
 
-        # description label
+        # Description label
         self.description = ("Before you begin, please run the OSNR Runelite client and log in with the credentials below.")
         self.label_description = customtkinter.CTkLabel(master=self,
                                                         text=self.description,
@@ -31,11 +37,13 @@ class DefaultView(customtkinter.CTkFrame):
         self.label_description.grid(row=2, column=0, sticky="nwes", padx=15)
 
         # Login Frame 2x3
-        self.login_frame = customtkinter.CTkFrame(master=self)
+        self.login_frame = customtkinter.CTkFrame(master=self, corner_radius=0)
         self.login_frame.grid(row=3, column=0, sticky="nwes", padx=40, pady=15)
-        self.login_frame.grid_columnconfigure(0, weight=1)
-        self.login_frame.grid_columnconfigure(1, weight=1)
-        self.login_frame.grid_columnconfigure(2, weight=0)  # For clipboard buttons
+        self.login_frame.grid_columnconfigure(0, weight=0, minsize=full_img_width)  # spacing
+        self.login_frame.grid_columnconfigure(1, weight=1)  # email
+        self.login_frame.grid_columnconfigure(2, weight=1)  # password
+        self.login_frame.grid_columnconfigure(3, weight=0, minsize=full_img_width)  # clipboard buttons
+
         self.login_frame.grid_rowconfigure(0, weight=0)
         self.login_frame.grid_rowconfigure(1, weight=0)
 
@@ -43,46 +51,47 @@ class DefaultView(customtkinter.CTkFrame):
         self.email = "osnrbot@gmail.com"
         self.password = "]7w(FS?z"
 
-        # Images
-        PATH = pathlib.Path(__file__).parent.parent.resolve()
-        img_size = 18
-        self.img_copy = ImageTk.PhotoImage(Image.open(f"{PATH}/images/copy.png").resize((img_size, img_size)), Image.ANTIALIAS)
-
         # Email
         self.lbl_email = customtkinter.CTkLabel(master=self.login_frame,
                                                 text=("Login: "),
                                                 text_font=("Roboto", 12))
-        self.lbl_email.grid(row=0, column=0, sticky="nes")
+        self.lbl_email.grid(row=0, column=1, sticky="nes")
 
         self.lbl_email_text = customtkinter.CTkLabel(master=self.login_frame,
                                                      text=self.email,
                                                      text_font=("Roboto", 12))
-        self.lbl_email_text.grid(row=0, column=1, sticky="nws")
+        self.lbl_email_text.grid(row=0, column=2, sticky="nws")
 
         self.btn_copy_email = customtkinter.CTkButton(master=self.login_frame,
                                                       text="",
-                                                      width=img_size+2,
+                                                      fg_color="gray40",
+                                                      hover_color="gray25",
+                                                      width=full_img_width,
+                                                      corner_radius=0,
                                                       image=self.img_copy,
                                                       command=self.copy_email_to_clipboard)
-        self.btn_copy_email.grid(row=0, column=2, sticky="ns")
+        self.btn_copy_email.grid(row=0, column=3, sticky="ns")
 
         # Password
         self.lbl_password = customtkinter.CTkLabel(master=self.login_frame,
                                                    text="Password: ",
                                                    text_font=("Roboto", 12))
-        self.lbl_password.grid(row=1, column=0, sticky="nes")
+        self.lbl_password.grid(row=1, column=1, sticky="nes")
 
         self.lbl_password_text = customtkinter.CTkLabel(master=self.login_frame,
                                                         text=self.password,
                                                         text_font=("Roboto", 12))
-        self.lbl_password_text.grid(row=1, column=1, sticky="nws")
+        self.lbl_password_text.grid(row=1, column=2, sticky="nws")
 
         self.btn_copy_password = customtkinter.CTkButton(master=self.login_frame,
                                                          text="",
-                                                         width=img_size+2,
+                                                         fg_color="gray40",
+                                                         hover_color="gray25",
+                                                         width=full_img_width,
+                                                         corner_radius=0,
                                                          image=self.img_copy,
                                                          command=self.copy_password_to_clipboard)
-        self.btn_copy_password.grid(row=1, column=2, sticky="ns")
+        self.btn_copy_password.grid(row=1, column=3, sticky="ns")
 
         # Copied label
         self.lbl_copy_text = customtkinter.CTkLabel(master=self,
