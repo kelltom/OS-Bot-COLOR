f3c4ea902b775c3d37536dfe16b4c712b5c8ccbf
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-07-27 14:37:37 -0400

    Emergency commit
    Risk of power outage

---
+++ src/model/alora_bot.py
@@ -0,0 +1,77 @@
+'''
+The AloraBot class contains properties and functions that are specific to the Alora client. This class should
+be inherited by Alora script classes.
+'''
+from abc import ABCMeta
+from model.runelite_bot import RuneliteBot
+from model.bot import Rectangle, Point
+import time
+
+
+class AloraBot(RuneliteBot, metaclass=ABCMeta):
+
+    # ------- Rects of Interest -------
+    rect_current_action = Rectangle(Point(10, 52), Point(171, 93))  # combat/skilling plugin text
+    rect_game_view = Rectangle(Point(9, 31), Point(517, 362))  # gameplay area
+    rect_hp = Rectangle(Point(526, 80), Point(552, 100))  # contains HP text value
+    rect_inventory = Rectangle(Point(554, 230), Point(737, 491))  # inventory area
+    # TODO: Add more rectangles of interest (prayer, spec, etc.)
+
+    # ------- Points of Interest -------
+    # --- Orbs ---
+    orb_compass = Point(x=571, y=48)
+    orb_prayer = Point(x=565, y=119)
+    orb_spec = Point(x=597, y=178)
+
+    # --- Control Panel (CP) ---
+    h1 = 213  # y-axis pixels to top of cp
+    h2 = 510  # y-axis pixels to bottom of cp
+    cp_combat = Point(x=545, y=h1)
+    cp_inventory = Point(x=646, y=h1)
+    cp_equipment = Point(x=678, y=h1)
+    cp_prayer = Point(x=713, y=h1)
+    cp_spellbook = Point(x=744, y=h1)
+    cp_logout = Point(x=646, y=h2)
+    cp_settings = Point(x=680, y=h2)
+
+    def is_in_combat(self) -> bool:
+        '''
+        Returns whether the player is in combat.
+        '''
+        result = self.get_text_in_rect(self.rect_current_action)
+        return result is not None
+
+    def toggle_auto_retaliate(self, toggle_on: bool):
+        '''
+        Toggles auto retaliate. Assumes client window is configured.
+        Args:
+            toggle_on: Whether to turn on or off.
+        '''
+        # click the combat tab
+        self.hc.move(self.cp_combat.x, self.cp_combat.y, duration=1)
+        self.hc.click()
+        time.sleep(0.5)
+
+        # Search for the auto retaliate button (deselected)
+        # If None, then auto retaliate is on.
+        auto_retal_btn = self.search_img_in_rect(f"{self.BOT_IMAGES}/cp_combat_autoretal.png", self.rect_inventory, conf=0.9)
+
+        if toggle_on and auto_retal_btn is not None or not toggle_on and auto_retal_btn is None:
+            self.hc.move(644, 402)
+            self.hc.click()
+        elif toggle_on:
+            print("Auto retaliate is already on.")
+        else:
+            print("Auto retaliate is already off.")
+
+    def teleport_home(self):
+        pass
+
+    def bank_at_home(self):
+        pass
+
+    def setup_alora(self):
+        '''
+        Sets up the Alora client.
+        '''
+        self.setup_client(window_title="Alora")
---
+++ src/model/alora_combat.py
@@ -0,0 +1,89 @@
+'''
+Combat bot for Alora. Kills tagged NPCs.
+'''
+from bot_utilities.runelite_utilities import is_in_combat
+from model.bot import BotStatus
+from model.alora_bot import AloraBot
+import time
+
+
+class AloraCombat(AloraBot):
+    def __init__(self):
+        title = "Combat Bot"
+        description = ("This bot kills tagged NPCs in Alora RSPS. Make sure to tag all NPCS " +
+                       "via Runelite before starting the bot!")
+        super().__init__(title=title, description=description)
+        # Create any additional bot options here. 'iterations' and 'current_iter' exist by default.
+
+    def save_options(self, options: dict):
+        '''
+        For each option in the dictionary, if it is an expected option, save the value as a property of the bot.
+        If any unexpected options are found, log a warning. If an option is missing, set the options_set flag to
+        False. No need to set bot status.
+        '''
+        for option in options:
+            if option == "iterations":
+                self.iterations = options[option]
+                self.log_msg(f"Iterations set to: {self.iterations}")
+            elif option == "multi_select_example":
+                self.multi_select_example = options[option]
+                self.log_msg(f"Multi-select example set to: {self.multi_select_example}")
+            elif option == "menu_example":
+                self.menu_example = options[option]
+                self.log_msg(f"Menu example set to: {self.menu_example}")
+            else:
+                self.log_msg(f"Unknown option: {option}")
+        self.options_set = True
+
+        # TODO: if options are invalid, set options_set flag to false
+
+        self.log_msg("Options set successfully.")
+
+    def main_loop(self):
+        '''
+        This function should consider the following:
+            - Should the bot bank?
+            - Should the bot loot?
+            - Should the bot heal?
+            - Should the bot pray?
+        '''
+        self.log_msg("Configuring client window...")
+        self.setup_alora()
+        self.log_msg("Client window configured.")
+
+        # Make sure auto retaliate is on
+        self.log_msg("Enabling auto retaliate...")
+        self.toggle_auto_retaliate(toggle_on=True)
+        time.sleep(1)
+
+        # Reselect inventory
+        self.hc.move(self.cp_inventory)
+        self.hc.click()
+
+        while self.current_iter < self.iterations:
+            if not self.status_check_passed():
+                return
+
+            # Attack NPC
+            if not self.is_in_combat():
+                self.log_msg("Attempting to attack NPC...")
+                if self.attack_nearest_tagged(self.rect_game_view):
+                    self.log_msg("NPC targetted.")
+                else:
+                    self.log_msg("No NPC found.")
+                time.sleep(2)
+
+            if not self.status_check_passed():
+                return
+
+            # If combat is over, assume we killed the NPC.
+            while self.is_in_combat():
+                time.sleep(1)
+                if not self.status_check_passed():
+                    return
+            self.increment_iter()
+            self.log_msg(f"Enemy killed. That's {self.current_iter}!")
+            time.sleep(1)
+
+        self.log_msg("Bot has completed all of its iterations.")
+        self.set_status(BotStatus.STOPPED)
