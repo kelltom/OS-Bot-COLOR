1b15114065fb44f3bbffa4c4fb48dd70f96b2a4e
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-07-28 13:32:37 -0400

    Removed TODO

--- src/view/home_view.py
+++ src/view/home_view.py
@@ -74,9 +74,6 @@ class HomeView(customtkinter.CTkFrame):
                                                    text="")
         self.label_status.grid(row=7, column=0, sticky="nwes")
 
-        # TODO: Make a big frame to replace this view telling the user how to
-        # configure their RS settings.
-
     def replace_settings(self):
         res = pag.confirm("Please close your game client before continuing.", title="Warning", buttons=["Done", "Cancel"])
         if res == "Cancel":
