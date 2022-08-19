2181d18aa8cccbbb1029bd7f0e6004be6c0b5f39
Kellen Evoy <44652363+kell90@users.noreply.github.com>
2022-08-19 14:52:03 -0400

    Improvements + support for Anglerfish

--- src/model/osnr/fishing.py
+++ src/model/osnr/fishing.py
@@ -10,36 +10,50 @@ import utilities.bot_cv as bcv
 
 class OSNRFishing(OSNRBot):
     def __init__(self):
-        title = "Fishing: Fly"
-        description = ("This bot power-fishes trout/salmon. Take out a fly fishing rod and feathers, position your character " +
+        title = "Fishing"
+        description = ("This bot fishes... fish. Take out a rod and bait, position your character " +
                        "near a fishing spot, and press play. Make sure the rest of your inventory is empty.")
         super().__init__(title=title, description=description)
         self.running_time = 0
+        self.fish_type = None
 
     def create_options(self):
         self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
+        self.options_builder.add_dropdown_option("fish_type", "What fish to catch?", ["Anglerfish", "Trout/Salmon"])
 
     def save_options(self, options: dict):
+        self.options_set = True
         for option in options:
             if option == "running_time":
                 self.running_time = options[option]
-                self.log_msg(f"Running time: {self.running_time} minutes.")
+            elif option == "fish_type":
+                if options[option] == "Anglerfish":
+                    self.fish_type = "anglerfish"
+                elif options[option] == "Trout/Salmon":
+                    self.fish_type = "salmon"
+                else:
+                    self.log_msg(f"Unknown fish type: {options[option]}")
+                    self.options_set = False
             else:
                 self.log_msg(f"Unknown option: {option}")
                 self.options_set = False
-                self.log_msg("Failed to set options.")
-                return
-        self.options_set = True
+        if not self.options_set:
+            self.log_msg("Failed to set options.")
+            return
+        self.log_msg(f"Running time: {self.running_time} minutes.")
+        self.log_msg(f"Fish type: {self.fish_type}.")
         self.log_msg("Options set successfully.")
 
     def main_loop(self):  # sourcery skip: low-code-quality, use-named-expression
         # Setup
-        self.setup_osnr(zoom_percentage=25)
+        self.setup_osnr(zoom_percentage=50)
 
         # Set compass
         self.mouse.move_to(self.orb_compass)
         self.mouse.click()
         time.sleep(0.5)
+
+        self.move_camera_up()
 
         last_inventory_pos = self.inventory_slots[6][3]
         last_inventory_rgb = pag.pixel(last_inventory_pos.x, last_inventory_pos.y)
@@ -58,18 +72,23 @@ class OSNRFishing(OSNRBot):
                 self.drop_inventory(skip_rows=1)
                 fished += 25
                 self.log_msg(f"Fishes fished: ~{fished}")
-                time.sleep(1)
-                continue
+                time.sleep(2)
 
             if not self.status_check_passed():
                 return
 
             # If not fishing, click fishing spot
-            is_fishing = bcv.search_text_in_rect(self.rect_current_action, ["fishing"], ["not"])
-            if not is_fishing:
-                spot = bcv.search_img_in_rect(f"{bcv.BOT_IMAGES}/near_reality/salmon_sprite.png", self.rect_game_view)
+            is_fishing = bcv.search_text_in_rect(self.rect_current_action, ["fishing", "fishirig"], ["not"])
+            while not is_fishing:
+                im_path = None
+                if self.fish_type == "anglerfish":
+                    im_path = f"{bcv.BOT_IMAGES}/near_reality/anglerfish_sprite.png"
+                elif self.fish_type == "salmon":
+                    im_path = f"{bcv.BOT_IMAGES}/near_reality/salmon_sprite.png"
+                spot = bcv.search_img_in_rect(im_path, self.rect_game_view)
                 if spot is None:
                     failed_searches += 1
+                    time.sleep(1)
                     if failed_searches > 10:
                         self.log_msg("Failed to find fishing spot.")
                         self.set_status(BotStatus.STOPPED)
@@ -77,8 +96,11 @@ class OSNRFishing(OSNRBot):
                 else:
                     self.mouse.move_to(spot)
                     pag.click()
-            time.sleep(5)
-
+                    break
+            time.sleep(3)
+            if not self.status_check_passed():
+                return
+            time.sleep(3)
             if not self.status_check_passed():
                 return
 
