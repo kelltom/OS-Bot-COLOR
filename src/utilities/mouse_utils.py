2df0f0efcdbb71c7fa26bc5f95cec4a41005f81f
Travis j Barton <39603097+Travis-Barton@users.noreply.github.com>
2022-09-21 10:54:42 -0700

    Added time variance and expanded mouse options

--- src/utilities/mouse_utils.py
+++ src/utilities/mouse_utils.py
@@ -1,40 +1,73 @@
 import pyautogui as pag
 import numpy as np
+import random as rd
+from typing import Callable
 
 
-class MouseUtils():
+class MouseUtils:
 
-    def move_to(self, point: tuple, duration=0.3, variance=0):
+    @staticmethod
+    def move_to(point: tuple, duration: float = 0.3, duration_variance: float = 0, time_variance: float = 0,
+                tween: Callable = None):
         '''
         Moves mouse to a point on screen with a random movement pattern.
         Args:
             point: x, y tuple of the destination point
             duration: duration of the movement
-            variance: maximum pixel variance in final x and y position
+            duration_variance: maximum pixel variance in final x and y position
+            time_variance: the variance absolute of duration. This number is always positive (gaussian with mean 0)
+            tween: a mouse movement object from pyautogui.
+                                If None, easeInOutSine is default.
+                                If 'rand' then a selection of random movements will be used
+                                else enter a callable pyautogui function
         '''
-        # tweens = [pag.easeOutBounce, pag.easeInBounce, pag.easeInBack,
-        #           pag.easeInCirc, pag.easeInCubic, pag.easeInElastic,
-        #           pag.easeInExpo, pag.easeInOutBounce, pag.easeInOutQuad,
-        #           pag.easeInOutQuart, pag.easeInQuart, pag.easeInQuint,
-        #           pag.easeInOutBack, pag.easeOutSine]
-        # tween = tweens[np.random.randint(0, len(tweens))]
-        tween = pag.easeInOutSine
+        if tween is None:
+            tween = pag.easeInOutSine
+        elif tween == 'rand':
+            tween = rd.choice([pag.easeOutBounce, pag.easeInBounce, pag.easeInBack,
+                               pag.easeInCirc, pag.easeInCubic, pag.easeInElastic,
+                               pag.easeInExpo, pag.easeInOutBounce, pag.easeInOutQuad,
+                               pag.easeInOutQuart, pag.easeInQuart, pag.easeInQuint,
+                               pag.easeInOutBack, pag.easeOutSine])
+        else:
+            if not isinstance(tween, Callable):
+                raise TypeError('mouse_movement must be a callable function. Use None for default or "rand" for random')
         x, y = point
-        if variance != 0:
-            x += np.random.randint(-variance, variance)
-            y += np.random.randint(-variance, variance)
-        pag.moveTo(x, y, duration=duration, tween=tween)
+        if duration_variance != 0:
+            x += np.random.randint(-duration_variance, duration_variance)
+            y += np.random.randint(-duration_variance, duration_variance)
+        pag.moveTo(x, y, duration=duration + np.abs(rd.gauss(0, time_variance)), tween=tween)
 
-    def move_rel(self, x, y, duration=0.3, variance=0):
+    def move_rel(self, x: int, y: int, duration: float = 0.3, duration_variance: float = 0, time_variance: float = 0,
+                 mouse_movement: Callable = None):
         '''
         Moves mouse relative to current position.
         Args:
             x: x distance to move
             y: y distance to move
             duration: duration of the movement
-            variance: maximum pixel variance in final x and y position
+            duration_variance: maximum pixel variance in final x and y position
+            time_variance: the variance absolute of duration. This number is always positive (gaussian with mean 0)
+            mouse_movement: a mouse movement object from pyautogui.
+                                If None, easeInOutSine is default.
+                                If 'rand' then a selection of random movements will be used
+                                else enter a callable pyautogui function
         '''
-        self.move_to((pag.position()[0] + x, pag.position()[1] + y), duration, variance)
+        self.move_to((pag.position()[0] + x, pag.position()[1] + y), duration, duration_variance, time_variance,
+                     mouse_movement)
 
-    def click(self):
+    @staticmethod
+    def click():
         pag.click()
+
+
+if __name__ == '__main__':
+    # testing
+    test_inst = MouseUtils()
+    test_inst.move_to((100, 100), 0.5, 10, 0.1, 'rand')
+    test_inst.move_to((500, 500), 0.5, 10, 0.1, pag.easeInQuart)
+    test_inst.move_to((300, 300), 0.5, 10, 0.1)
+
+    test_inst.move_rel(100, 100, 0.5, 10, 0.1, 'rand')
+    test_inst.move_rel(-100, 100, 0.5, 10, 0.1, pag.easeInQuart)
+    test_inst.move_rel(100, -100, 0.5, 10, 0.1)
