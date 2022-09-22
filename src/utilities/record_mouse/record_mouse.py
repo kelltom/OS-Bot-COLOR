bb342b3c0804c0cec44720b2bb7c0f6f412f8cc4
Kellen Evoy <kellenevoy@gmail.com>
2022-09-22 13:39:09 -0400

    Combined results to a single folder
    Things I changed:
    - I moved this script into its own folder within utilities
    - Results from running this script (screenshots + CSV) will be created in a timestamped file within this new folder
    - Screenshots are numbered matching the corresponding index in the CSV file
    
    I think what you had before for screenshots was overwriting itself as it was just returning the type of the data record, so there would only ever be one screenshot there. I figured that if we just number them to match the CSV, we can easily lookup which click belongs to which screenshot.

--- src/utilities/record_mouse/record_mouse.py
+++ src/utilities/record_mouse/record_mouse.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 import pandas as pd
 import time
+import pathlib
 from pynput import mouse
 import pyautogui as pg
 import os
-import warnings
-if not os.path.exists('auto_screenshots'):
-    os.mkdir('auto_screenshots')
 
+# Create folder to contain results
+PATH = pathlib.Path(__file__).parent.resolve().joinpath(datetime.now().strftime("%I_%M_%S_%p"))
+if not os.path.exists(PATH):
+    os.makedirs(PATH)
+    os.makedirs(f'{PATH}\\screenshots')
+PATH = str(PATH).replace('\\', '/')
 
 def on_click(x, y, button, pressed):
     """
@@ -23,7 +28,7 @@ def on_click(x, y, button, pressed):
         s = time.time()
         mouse_loc = pg.position()
         try:
-            df = pd.read_csv('click_log.csv', index_col=0)
+            df = pd.read_csv(f'{PATH}/click_log.csv', index_col=0)
         except FileNotFoundError:
             df = pd.DataFrame(columns=["x", "y", "time"], index=[0])
 
@@ -31,9 +36,9 @@ def on_click(x, y, button, pressed):
         box = (mouse_loc[0] - frame_diff, mouse_loc[1] - frame_diff,
                frame_diff * 2, frame_diff * 2)
         df = pd.concat([df, pd.DataFrame({'x': x, 'y': y, 'time': s}, index=[0])], axis=0)
-        pg.screenshot(f'auto_screenshots/mouse_shot_{df.tail(1).index + 1}.png', region=box)
+        pg.screenshot(f'{PATH}/screenshots/mouse_shot_{len(df.index)-1}.png', region=box)
         df.reset_index(inplace=True, drop=True)
-        df.to_csv('click_log.csv')
+        df.to_csv(f'{PATH}/click_log.csv')
         print(df)
         return True  # Returning False if you need to stop the program when Left clicked.
     elif button == mouse.Button.right and pressed:
@@ -42,7 +47,7 @@ def on_click(x, y, button, pressed):
 
 
 record = pd.DataFrame(columns=["x", "y", "time"])
-record.to_csv('click_log.csv')
+record.to_csv(f'{PATH}/click_log.csv')
 listener = mouse.Listener(on_click=on_click)
 listener.start()
 listener.join()
