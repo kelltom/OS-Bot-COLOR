7d9b40437c7ec793862a9391f6dd50008b9a310e
enkidu <a7made_derazi@hotmail.com>
2022-12-10 14:46:36 +0300

    extra lists with item ID
    mining & woodcutting tools as well as their respective resources item ID

--- src/utilities/api/item_ids.py
+++ src/utilities/api/item_ids.py
@@ -9,4 +9,62 @@ food_list = [
     7946, # monkfish
     3144, # karambwan
     385, # shark
+    2142, #Cooked meat
+]
+
+
+WoodCutting_axe_list= [
+    1351,  #Bronze axe
+    1349,  #Iron axe
+    1353,  #Steel axe
+    1355,  #Mithril axe
+    1357,  #Adamant axe
+    1359,  #Rune axe
+    6739,  #Dragon axe
+    23862, #Crystal axe
+    23673, #Crystal axe
+
+]
+
+pickaxe_list=[
+    1265,	#Bronze pickaxe
+    1267,	#Iron pickaxe
+    1269,	#Steel pickaxe
+    1273,	#Mithril pickaxe
+    1271,	#Adamant pickaxe
+    1275,	#Rune pickaxe
+    11920,	#Dragon pickaxe
+    12797,	#Dragon pickaxe
+    23680,	#Crystal pickaxe
+    23863,	#Crystal pickaxe
+
+]
+
+wood_type_list=[
+    1511,	#Logs
+    1521,	#Oak logs
+    1519,	#Willow logs
+    1517,	#Maple logs
+    6333,	#Teak logs
+    6332,	#Mahogany logs
+    13355,	#Juniper logs
+    10810,	#Arctic pine logs
+    1515,	#Yew logs
+    1513,	#Magic logs
+    19669,	#Redwood logs
+
+]
+
+ore_type_list=[
+    434,	#Clay
+    436,	#Copper ore
+    438,	#Tin ore
+    440,	#Iron ore
+    442,	#Silver ore
+    453,	#Coal
+    444,	#Gold ore
+    447,	#Mithril ore
+    449,	#Adamantite ore
+    451,	#Runite ore
+    21347,	#Amethyst
 ]
