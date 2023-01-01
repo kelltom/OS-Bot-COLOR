e36d09505f0f4d121b2504159f7d20db45a75ec7
Kellen Evoy <kellenevoy@gmail.com>
2023-01-01 16:54:41 -0500

    Create template.py

---
+++ src/model/osrs/template.py
@@ -0,0 +1,61 @@
+import time
+
+import utilities.api.item_ids as ids
+import utilities.color as clr
+import utilities.random_util as rd
+from model.osrs.osrs_bot import OSRSBot
+from model.runelite_bot import BotStatus
+from utilities.api.morg_http_client import MorgHTTPSocket
+from utilities.api.status_socket import StatusSocket
+
+
+class OSRSTemplate(OSRSBot):
+    def __init__(self):
+        bot_title = "<Bot name here"
+        description = "Bot description here."
+        super().__init__(bot_title=bot_title, description=description)
+        # Set option variables below (initial value is only used during UI-less testing)
+        self.running_time = 1
+        self.take_breaks = True
+
+    def create_options(self):
+        self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
+        self.options_builder.add_checkbox_option("take_breaks", "Take breaks?", [" "])
+
+    def save_options(self, options: dict):
+        for option in options:
+            if option == "running_time":
+                self.running_time = options[option]
+            elif option == "take_breaks":
+                self.take_breaks = options[option] != []
+            else:
+                self.log_msg(f"Unknown option: {option}")
+                print("Developer: ensure that the option keys are correct, and that options are being unpacked correctly.")
+                self.options_set = False
+                return
+        self.log_msg(f"Running time: {self.running_time} minutes.")
+        self.log_msg(f"Bot will{' ' if self.take_breaks else ' not '}take breaks.")
+        self.log_msg("Options set successfully.")
+        self.options_set = True
+
+    def main_loop(self):
+        # Setup APIs
+        api_m = MorgHTTPSocket()
+        api_s = StatusSocket()
+
+        # Main loop
+        start_time = time.time()
+        end_time = self.running_time * 60
+        while time.time() - start_time < end_time:
+
+            # 5% chance to take a break between clicks
+            if rd.random_chance(probability=0.05) and self.take_breaks:
+                self.take_break(max_seconds=15)
+
+            # -- Perform bot actions here --
+
+            self.update_progress((time.time() - start_time) / end_time)
+
+        self.update_progress(1)
+        self.log_msg("Finished.")
+        self.set_status(BotStatus.STOPPED)
