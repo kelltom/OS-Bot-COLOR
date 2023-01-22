94b658c45a6966b6c90066b8731d446682219f4a
Kellen Evoy <kellenevoy@gmail.com>
2023-01-22 12:47:37 -0500

    Linting & Pre-commit CI Check
    - Some minor adjustments made for performance/readability (e.g., magic function logged a message with "inventory" as text, some conditionals having redundant "== True" checks)

--- src/utilities/control_panel.py
+++ src/utilities/control_panel.py
@@ -1,43 +1,45 @@
-import utilities.imagesearch as imsearch
-from utilities.random_util import random_chance
-from model.osrs.osrs_bot import OSRSBot
 from typing import Union
-from pyautogui import keyDown,keyUp 
+
+from pyautogui import keyDown, keyUp
+
+import utilities.imagesearch as imsearch
+from model.osrs.osrs_bot import OSRSBot
+from utilities.random_util import random_chance
 
 
 class InvOpen(object):
     def __init__(self, osrs_bot: OSRSBot):
         self.osrs_bot = osrs_bot
+
     def __enter__(self):
-        opened = _ensure_inventory_open(self.osrs_bot)
-        return opened
+        return _ensure_inventory_open(self.osrs_bot)
+
     def __exit__(self, *args, **kwargs):
         pass
+
 
 class MagicOpen(object):
     def __init__(self, osrs_bot: OSRSBot):
         self.osrs_bot = osrs_bot
+
     def __enter__(self):
-        opened = _ensure_magic_tab_open(self.osrs_bot)
-        return opened
+        return _ensure_magic_tab_open(self.osrs_bot)
+
     def __exit__(self, *args, **kwargs):
         pass
 
 
-def _get_active_tab(osrs_bot: OSRSBot) -> Union[int, None]:
+def get_active_tab(osrs_bot: OSRSBot) -> Union[int, None]:
     """
     Searches through the tabs in the player control panel and returns the index of the active tab or None if none was found
 
     osrs_bot: Reference to an OSRSBot instance
     """
-    for i in range(0,14):
+    return next(
+        (i for i in range(14) if _check_if_tab_active(osrs_bot=osrs_bot, tab=i)),
+        None,
+    )
 
-        is_active = _check_if_tab_active(osrs_bot=osrs_bot, tab = i)
-
-        if is_active == True:
-            return i
-
-    return None
 
 def _check_if_tab_active(osrs_bot: OSRSBot, tab: int) -> bool:
     """
@@ -47,10 +49,11 @@ def _check_if_tab_active(osrs_bot: OSRSBot, tab: int) -> bool:
     tab: integer value of the control panel tab you would like to check if is open
     """
 
-    inventory_path = imsearch.BOT_IMAGES.joinpath("control_panel","selected.png")
-    inv = imsearch.search_img_in_rect(image=inventory_path, rect=osrs_bot.win.cp_tabs[tab], confidence = 0.35)
+    inventory_path = imsearch.BOT_IMAGES.joinpath("control_panel", "selected.png")
+    inv = imsearch.search_img_in_rect(image=inventory_path, rect=osrs_bot.win.cp_tabs[tab], confidence=0.35)
 
-    return True if inv else False
+    return bool(inv)
+
 
 def _ensure_inventory_open(osrs_bot: OSRSBot) -> bool:
     """
@@ -61,21 +64,21 @@ def _ensure_inventory_open(osrs_bot: OSRSBot) -> bool:
 
     inv_open = _check_if_tab_active(osrs_bot, 3)
 
-    if inv_open == False:
-
+    if inv_open:
         osrs_bot.log_msg("Inventory not detected as open")
 
-        if random_chance(0.1) == True:
+        if random_chance(0.1):
             osrs_bot.mouse.move_to(osrs_bot.win.cp_tabs[3].random_point())
             osrs_bot.mouse.click()
 
         else:
-            keyDown('esc')
-            keyUp('esc')
+            keyDown("esc")
+            keyUp("esc")
 
-        inv_open = _check_if_tab_active(osrs_bot,  3)
+        inv_open = _check_if_tab_active(osrs_bot, 3)
 
     return inv_open
+
 
 def _ensure_magic_tab_open(osrs_bot: OSRSBot):
     """
@@ -84,21 +87,19 @@ def _ensure_magic_tab_open(osrs_bot: OSRSBot):
     return: True/False whether the tab has been succesfully opened
     """
 
-    open = _check_if_tab_active(osrs_bot, 6)
+    spellbook_open = _check_if_tab_active(osrs_bot, 6)
 
-    if open == False:
+    if not spellbook_open:
+        osrs_bot.log_msg("Spellbook not detected as open")
 
-        osrs_bot.log_msg("Inventory not detected as open")
-
-        if random_chance(0.1) == True:
+        if random_chance(0.1):
             osrs_bot.mouse.move_to(osrs_bot.win.cp_tabs[6].random_point())
             osrs_bot.mouse.click()
 
         else:
-            keyDown('f6')
-            keyUp('f6')
+            keyDown("f6")
+            keyUp("f6")
 
-        open = _check_if_tab_active(osrs_bot,  6)
+        spellbook_open = _check_if_tab_active(osrs_bot, 6)
 
-    return open
-
+    return spellbook_open
