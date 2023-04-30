ba90e13e28d8793aaa8c4e12c4d0795ed98c50e2
Kellen Evoy <44652363+kelltom@users.noreply.github.com>
2023-04-30 15:22:34 -0400

    Added ImageType enum, fixed query formatting, added some error handling

--- src/utilities/sprite_scraper.py
+++ src/utilities/sprite_scraper.py
@@ -4,13 +4,13 @@ This utility does not work well with IPv6. If you are having issues, try disabli
 """
 
 import os
+import re
+from enum import Enum
 from typing import List
-from urllib.parse import urljoin
 
 import cv2
 import numpy as np
 import requests
-import re
 
 if __name__ == "__main__":
     import sys
@@ -20,6 +20,12 @@ if __name__ == "__main__":
 import utilities.imagesearch as imsearch
 
 DEFAULT_DESTINATION = imsearch.BOT_IMAGES.joinpath("scraper")
+
+
+class ImageType(Enum):
+    NORMAL = 0
+    BANK = 1
+    ALL = 2
 
 
 class SpriteScraper:
@@ -32,7 +38,7 @@ class SpriteScraper:
         Args:
             search_string: A comma-separated list of wiki keywords to locate images for.
         Keyword Args:
-            image_type: 0 = Normal, 1 = Bank, 2 = Both. Normal sprites are full-size, and bank sprites are cropped at the top
+            image_type: 0 = NORMAL, 1 = BANK, 2 = ALL. Normal sprites are full-size, and bank sprites are cropped at the top
                         to improve image search performance within the bank interface (crops out stack numbers). Default is 0.
             destination: The folder to save the downloaded images to. Default is defined in the global `DEFAULT_DESTINATION`.
             notify_callback: A function (usually defined in the view) that takes a string as a parameter. This function is
@@ -42,17 +48,17 @@ class SpriteScraper:
             >>> scraper = SpriteScraper()
             >>> scraper.search_and_download(
             >>>     search_string = "molten glass, bucket of sand",
-            >>>     image_type = 1,
+            >>>     image_type = ImageType.BANK,
             >>>     destination = imsearch.BOT_IMAGES.joinpath("bank"),
             >>>     notify_callback = self.log_msg,
             >>> )
         """
-        image_type = kwargs.get("image_type", 0)
+        image_type = kwargs.get("image_type", ImageType.NORMAL)
         destination = kwargs.get("destination", DEFAULT_DESTINATION)
         notify_callback = kwargs.get("notify_callback", print)
 
         # Ensure the iamge_type is valid
-        if image_type not in (0, 1, 2):
+        if isinstance(image_type, ImageType):
             notify_callback("Invalid image type argument.")
             return
 
@@ -68,128 +74,42 @@ class SpriteScraper:
         while i < len(img_names) - 1:
             i += 1
 
+            # Fix capitalization
+            img_names[i] = self.capitalize_each_word(img_names[i])
             notify_callback(f"Searching for {img_names[i]}...")
-            img_url = self.sprite_url(img_names[i])
-            
+
+            # Get image URL
+            img_url = self.__sprite_url(img_names[i])
             if img_url is None:
                 notify_callback(f"No image found for {img_names[i]}.\n")
                 continue
-
             notify_callback("Found image.")
 
             # Download image
             notify_callback("Downloading image...")
             try:
                 downloaded_img = self.__download_image(img_url)
-            except Exception as e:
-                notify_callback(f"Error: {e}\n")
+            except requests.exceptions.RequestException as e:
+                notify_callback(f"Network error: {e}\n")
+                continue
+            except cv2.error as e:
+                notify_callback(f"Image decoding error: {e}\n")
                 continue
 
             # Save image according to image_type argument
             filepath = destination.joinpath(img_names[i])
-            if image_type in {0, 2}:
+            if image_type in {ImageType.NORMAL, ImageType.ALL}:
                 cv2.imwrite(f"{filepath}.png", downloaded_img)
                 nl = "\n"
                 notify_callback(f"Success: {img_names[i]} sprite saved.{nl if image_type != 2 else ''}")
-            if image_type in {1, 2}:
-                cropped_img = self.__bankify_image(downloaded_img)
+            if image_type in {ImageType.BANK, ImageType.ALL}:
+                cropped_img = self.bankify_image(downloaded_img)
                 cv2.imwrite(f"{filepath}_bank.png", cropped_img)
                 notify_callback(f"Success: {img_names[i]} bank sprite saved.\n")
 
         notify_callback(f"Search complete. Images saved to:\n{destination}.\n")
 
-    def capitalize_each_in(self, string: str) -> str:
-        """
-        Capitalizes the first letter of each word in a string of words separated by underscores, retaining the
-        underscores.
-        """
-        exclude = ["from", "of", "to", "in", "with", "on", "at", "by", "for"]
-        return "_".join(word if word in exclude else word.capitalize() for word in string.split("_"))
-
-    def format_args(self, string: str) -> List[str]:
-        """
-        Formats a comma-separated list of strings into a list of strings where each string is capitalized and
-        underscores are used instead of spaces.
-        """
-        # If the string is empty, return an empty list
-        if not string.strip():
-            return []
-        # Reduce multiple spaces to a single space
-        string = " ".join(string.split())
-        # Strip whitespace and replace spaces with underscores
-        return [word.strip().replace(" ", "_").capitalize() for word in string.split(",")]
-    
-    def insert_underscores(self, string: str) -> str:
-        """
-        If the item has spaces it will replace them with underscores.
-        Args:
-            string: String you want to input underscores to.
-        Return:
-            Returns the string with underscores within it.
-        """
-        if ' ' in string:
-            return string.replace(" ", "_")
-        else:
-            return string
-
-    def sprite_url(self, item: str) -> str:
-        """
-        Returns the sprite URL of the item provided.
-        Args:
-            item: The item name.
-        Returns:
-            URL of the sprite image (string)
-        """
-        info_box = self.item_info_box(item)
-        if info_box is None:
-            print("page doesn't exist")
-            return None
-        pattern = r'\[\[File:(.*?)\]\]'
-        match = re.search(pattern, info_box)
-
-        if match:
-            filename = match.group(1)
-            filename = self.insert_underscores(filename)
-            return self.base_url + "images/" + filename
-
-        else:
-            print("Sprite couldn't be found in the info box.")
-            return None
-
-
-    def item_info_box(self, item: str) -> str:
-        """
-        Returns a string of data from the info box for a specific item from the Oldschool 
-        Runescape Wiki.
-        Args:
-            item: The item name.
-        Returns:
-            String of json data of the info box.
-        """
-        params = {
-        "action": "query",
-        "prop": "revisions",
-        "rvprop": "content",
-        "format": "json",
-        "titles": item
-        }
-
-        try:
-            response = requests.get(url=self.base_url + "/api.php", params=params)
-            data = response.json()
-            pages = data["query"]["pages"]
-            page_id = list(pages.keys())[0]
-            if int(page_id) < 0:
-                return None
-            return pages[page_id]["revisions"][0]['*']
-        except requests.exceptions.ConnectionError as e:
-            print("Network error:", e)
-            return None
-        except requests.exceptions.RequestException as e:
-            print("Request failed:", e)
-            return None
-
-    def __bankify_image(self, image: cv2.Mat) -> cv2.Mat:
+    def bankify_image(self, image: cv2.Mat) -> cv2.Mat:
         """
         Converts a sprite into an image that is suitable for image searching within a bank interface.
         This function centers the image in a 36x32 frame, and deletes some pixels at the top of the image to
@@ -212,6 +132,27 @@ class SpriteScraper:
         image[:9, :] = 0
         return image
 
+    def capitalize_each_word(self, string: str) -> str:
+        """
+        Capitalizes the first letter of each word in a string of words separated by underscores, retaining the
+        underscores.
+        """
+        exclude = ["from", "of", "to", "in", "with", "on", "at", "by", "for"]
+        return "_".join(word if word in exclude else word.capitalize() for word in string.split("_"))
+
+    def format_args(self, string: str) -> List[str]:
+        """
+        Formats a comma-separated list of strings into a list of strings where each string is capitalized and
+        underscores are used instead of spaces.
+        """
+        # If the string is empty, return an empty list
+        if not string.strip():
+            return []
+        # Reduce multiple spaces to a single space
+        string = " ".join(string.split())
+        # Strip whitespace and replace spaces with underscores
+        return [word.strip().replace(" ", "_").capitalize() for word in string.split(",")]
+
     def __download_image(self, url: str) -> cv2.Mat:
         """
         Downloads an image from a URL.
@@ -225,6 +166,66 @@ class SpriteScraper:
         downloaded_img = cv2.imdecode(downloaded_img, cv2.IMREAD_UNCHANGED)
         return downloaded_img
 
+    def __insert_underscores(self, string: str) -> str:
+        """
+        If the item has spaces it will replace them with underscores.
+        Args:
+            string: String you want to input underscores to.
+        Return:
+            Returns the string with underscores within it.
+        """
+        return string.replace(" ", "_") if " " in string else string
+
+    def __item_info_box(self, item: str) -> str:
+        """
+        Returns a string of data from the info box for a specific item from the Old School
+        RuneScape Wiki.
+        Args:
+            item: The item name.
+        Returns:
+            String of json data of the info box or None if the item does not exist or if an error occurred.
+        """
+        params = {"action": "query", "prop": "revisions", "rvprop": "content", "format": "json", "titles": item}
+
+        try:
+            response = requests.get(url=self.base_url + "/api.php", params=params)
+            data = response.json()
+            pages = data["query"]["pages"]
+            page_id = list(pages.keys())[0]
+            if int(page_id) < 0:
+                return None
+            return pages[page_id]["revisions"][0]["*"]
+        except requests.exceptions.ConnectionError as e:
+            print("Network error:", e)
+            return None
+        except requests.exceptions.RequestException as e:
+            print("Request failed:", e)
+            return None
+
+    def __sprite_url(self, item: str) -> str:
+        """
+        Returns the sprite URL of the item provided.
+        Args:
+            item: The item name.
+        Returns:
+            URL of the sprite image (string)
+        """
+        info_box = self.__item_info_box(item)
+        if info_box is None:
+            print("page doesn't exist")
+            return None
+        pattern = r"\[\[File:(.*?)\]\]"
+        match = re.search(pattern, info_box)
+
+        if match:
+            filename = match.group(1)
+            filename = self.__insert_underscores(filename)
+            return self.base_url + "images/" + filename
+
+        else:
+            print("Sprite couldn't be found in the info box.")
+            return None
+
 
 if __name__ == "__main__":
     scraper = SpriteScraper()
@@ -235,12 +236,12 @@ if __name__ == "__main__":
     assert scraper.format_args(" swordfish ,lobster, lobster   pot ") == ["Swordfish", "Lobster", "Lobster_pot"]
     assert scraper.format_args("Swordfish ,lobster, Lobster_Pot ") == ["Swordfish", "Lobster", "Lobster_pot"]
 
-    assert scraper.capitalize_each_in("swordfish") == "Swordfish"
-    assert scraper.capitalize_each_in("Lobster_pot") == "Lobster_Pot"
-    assert scraper.capitalize_each_in("arceuus_home_teleport") == "Arceuus_Home_Teleport"
-    assert scraper.capitalize_each_in("protect_from_magic") == "Protect_from_Magic"
-    assert scraper.capitalize_each_in("teleport_to_house") == "Teleport_to_House"
-    assert scraper.capitalize_each_in("claws_of_guthix") == "Claws_of_Guthix"
+    assert scraper.capitalize_each_word("swordfish") == "Swordfish"
+    assert scraper.capitalize_each_word("Lobster_pot") == "Lobster_Pot"
+    assert scraper.capitalize_each_word("arceuus_home_teleport") == "Arceuus_Home_Teleport"
+    assert scraper.capitalize_each_word("protect_from_magic") == "Protect_from_Magic"
+    assert scraper.capitalize_each_word("teleport_to_house") == "Teleport_to_House"
+    assert scraper.capitalize_each_word("claws_of_guthix") == "Claws_of_Guthix"
 
     scraper.search_and_download(
         search_string=" lobster , lobster  Pot",
